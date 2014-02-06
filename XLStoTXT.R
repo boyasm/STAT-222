a0b3c00dc944750b157a8e5d6eaebc8381a1bf2a
MATTHEW GEORGE BOYAS <mboyas@s222-1.(none)>
2014-02-05 21:23:08 -0700

    modified xls->txt.R program to replace missing with NA; updated SCAD.txt

--- XLStoTXT.R
+++ XLStoTXT.R
@@ -1,6 +1,7 @@
 XLStoTXT <- function(path='~/STAT-222/Raw Datasets/SCAD 3.0/SCAD 3.0 1990-2011.xls'){
   require('gdata')
   SCAD <- read.xls(path, method='tab')
+  SCAD[SCAD==""] <- NA
   txtPath <- gsub('.xls', '.txt', path)
   write.table(SCAD, txtPath, sep='\t')
 }
