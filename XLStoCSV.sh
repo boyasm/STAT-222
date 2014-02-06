09d3082ec6d1c8d7f85a501aedc5e83d3aba00d2
MATTHEW GEORGE BOYAS <mboyas@s222-1.(none)>
2014-02-05 19:10:02 -0700

    added FINAL xls to txt script (.R) and the corresponding tab-delimited text file

--- XLStoCSV.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd Raw\ Datasets/SCAD\ 3.0
-mv SCAD\ 3.0\ 1990-2011.xls SCAD\ 3.0\ 1990-2011.csv
---
+++ XLStoTXT.R
@@ -0,0 +1,6 @@
+XLStoTXT <- function(path='~/STAT-222/Raw Datasets/SCAD 3.0/SCAD 3.0 1990-2011.xls'){
+  require('gdata')
+  SCAD <- read.xls(path, method='tab')
+  txtPath <- gsub('.xls', '.txt', path)
+  write.table(SCAD, txtPath, sep='\t')
+}
