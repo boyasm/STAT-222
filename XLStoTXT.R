874521f319d002f93db2df189c26924595674680
Matt Boyas <boyasm@msn.com>
2014-02-25 19:51:19 -0800

    Added new raw datasets, modified XLStoTXT.R

--- XLStoTXT.R
+++ XLStoTXT.R
@@ -1,7 +1,7 @@
-XLStoTXT <- function(path='~/STAT-222/Raw Datasets/SCAD 3.0/SCAD 3.0 1990-2011.xls'){
+XLStoTXT <- function(path){
   require('gdata')
-  SCAD <- read.xls(path, method='tab')
-  SCAD[SCAD==""] <- NA
+  data <- read.xls(path, method='tab')
+  data[data==""] <- NA
   txtPath <- gsub('.xls', '.txt', path)
-  write.table(SCAD, txtPath, sep='\t')
+  write.table(data, txtPath, sep='\t')
 }
