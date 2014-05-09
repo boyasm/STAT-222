7a5224371c7c1c89da33d09d15f731a92c392f43
Matt Boyas <boyasm@msn.com>
2014-05-09 15:32:51 -0700

    organized folder structure

--- XLStoTXT.R
+++
@@ -1,7 +0,0 @@
-XLStoTXT <- function(path){
-  require('gdata')
-  data <- read.xls(path, method='tab')
-  data[data==""] <- NA
-  txtPath <- gsub('.xls', '.txt', path)
-  write.table(data, txtPath, sep='\t')
-}
